a1036f5cca2c899a7a733f6ec48aa7e623cf9f71
Joaquin Corradi <corradijoaquin@gmail.com>
2023-07-12 23:22:57 -0300

    Agregando modo claro, todavia en desarrollo

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,4 @@
+// Include a menu icon on smaller devices
 let menu = document.querySelector(".menu-icon");
 let nav = document.querySelector(".nav");
 
@@ -6,6 +7,54 @@ menu.onclick = () => {
   nav.classList.toggle("open")
 }
 
+// Scroll back to top when button is clicked
 function backToTop() {
   scrollTo(0, 0);
 }
+
+// Toggle lightmode
+let themeButton = document.querySelector("#theme-button");
+let body = document.querySelector("body");
+let icon = document.getElementById("lightmode-icon");
+let li = document.getElementsByTagName("li");
+let a = document.getElementsByTagName("a")
+let icons = document.getElementsByTagName("i");
+
+themeButton.addEventListener("click", e => {
+  body.classList.toggle("lightmode");
+
+  if (body.classList.contains('lightmode')) {
+    for (var i = 0; i < li.length; i++) {
+      li[i].classList.add('lightmode');
+    }
+
+    for (var i = 0; i < a.length; i++) {
+      a[i].classList.add('lightmode');
+    }
+
+    for (var i = 0; i < icons.length; i++) {
+      icons[i].classList.add('lightmode');
+    }
+
+  } else {
+    for (var i = 0; i < li.length; i++) {
+      li[i].classList.remove('lightmode');
+    }
+
+    for (var i = 0; i < a.length; i++) {
+      a[i].classList.remove('lightmode');
+    }
+
+    for (var i = 0; i < icons.length; i++) {
+      icons[i].classList.remove('lightmode');
+    }
+  }
+
+  if (!body.classList.contains("lightmode")) {
+    icon.classList.remove("ri-sun-line");
+    icon.classList.add("ri-moon-line");
+  } else {
+    icon.classList.remove("ri-moon-line");
+    icon.classList.add("ri-sun-line")
+  }
+});
