6d14ef7e51ebfc397a923c86dccb22ad4172254a
Joaquin I. Corradi (Fedora Linux) <corradijoaquin@gmail.com>
2023-08-05 15:33:18 -0300

    Form now working

---
+++ assets/js/form.js
@@ -0,0 +1,20 @@
+function sendEmail() {
+  const body =
+    "El siguiente es un mensaje enviado desde la pagina web www.doctorcorradi.com.ar <br/> <br/> <br/>" +
+    "Datos del usuario: <br/> <br/>" +
+    "Nombre: " +
+    document.getElementById("nombre").value +
+    "<br/> E-mail: " +
+    document.getElementById("email").value +
+    "<br/> Mensaje: " +
+    document.getElementById("mensaje").value +
+    "<br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/>";
+
+  Email.send({
+    SecureToken: "1f0c1ffa-b77d-46c8-b62e-1ff5b656f05f",
+    To: "joakoalternativa2@gmail.com",
+    From: "nuevomensajedoctorcorradi@gmail.com",
+    Subject: "Nuevo mensaje de doctorcorradi.com.ar",
+    Body: body,
+  });
+}
